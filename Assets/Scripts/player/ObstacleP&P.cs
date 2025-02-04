4e24fd8f72fc4a48cfb1378eedba159ba1bc2f56
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-02-04 16:17:03 +0100

    sound & rework
    Done:
     - sound import
     - obstacle rework
     - use of codecks
    
    Note* use codecks more

--- Assets/Scripts/player/ObstacleP&P.cs
+++ Assets/Scripts/player/ObstacleP&P.cs
@@ -4,40 +4,48 @@ public class ObstaclePP : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D player;
 
-    [Header("Vars")]
-    [SerializeField] private float pushForce;
-    [SerializeField] private float pullForce;
+    [Header("Push")]
+
+    [SerializeField] private float pushForceL1;
+    [SerializeField] private float pushForceL2;
+    [SerializeField] private float pushForceL3;
+
+    [Header("Pull")]
+    [SerializeField] private float pullForceL1;
+    [SerializeField] private float pullForceL2;
+    [SerializeField] private float pullForceL3;
+
     void Start()
     {
         if (player == null)
-            player = this.GetComponent<Rigidbody2D>();
+            player = GetComponent<Rigidbody2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("pushG"))
         {
-            player.AddForce(other.transform.up * 5);
+            player.AddForce(other.transform.up * pushForceL1);
         }
         else if (other.gameObject.CompareTag("pushB"))
         {
-            player.AddForce(other.transform.up * 10);
+            player.AddForce(other.transform.up * pushForceL2);
         }
         else if (other.gameObject.CompareTag("pushR"))
         {
-            player.AddForce(other.transform.up * 15);
+            player.AddForce(other.transform.up * pushForceL3);
         }
         else if (other.gameObject.CompareTag("pullG"))
         {
-            player.AddForce(-other.transform.up * 5);
+            player.AddForce(-other.transform.up * pullForceL1);
         }
         else if (other.gameObject.CompareTag("pullB"))
         {
-            player.AddForce(-other.transform.up * 10);
+            player.AddForce(-other.transform.up * pullForceL2);
         }
         else if (other.gameObject.CompareTag("pullR"))
         {
-            player.AddForce(-other.transform.up * 15);
+            player.AddForce(-other.transform.up * pullForceL3);
         }
     }
 }
